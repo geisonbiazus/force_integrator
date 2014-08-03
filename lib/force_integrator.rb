c1bd8515b2660eaef32ae6e002a6da655736759e
Geison Biazus <geisonbiazus@gmail.com>
2014-08-03 18:29:53 -0300

    bug

--- lib/force_integrator.rb
+++ lib/force_integrator.rb
@@ -4,6 +4,7 @@ require 'sidekiq'
 require "force_integrator/version"
 require "force_integrator/authenticator"
 require "force_integrator/models"
+require "force_integrator/field_mapper"
 require "force_integrator/field"
 require "force_integrator/integrator"
 require 'force_integrator/mapper'
